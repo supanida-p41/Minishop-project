8a0596f9771e78b4bdbb8c1428c61d63ca1dfd86
supanida-p41 <supanida.p@betimes.biz>
2022-12-28 15:36:04 +0700

    update commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
